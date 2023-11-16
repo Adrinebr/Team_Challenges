9ceed414923dec509fcaa32d87ccc55a75af83dd
Ángela López Molina <147253096+AngelaLM14@users.noreply.github.com>
2023-11-16 20:01:21 +0100

    Avances sesion 16.11

--- clase.py
+++ clase.py
@@ -1,5 +1,49 @@
-import variables.py
-class tablero
+import funciones as fun
+import numpy as np
+import uuid as uuid
+import variables as var
+class tablero: 
+    dimensiones = [var.lado, var.lado]
+    def __init__ (self,dimensiones,id_jugador,barcos=[]):
+        self.dimensiones = dimensiones
+        self.barcos = barcos
+        self.id = id_jugador
+
+    def crear_tablero(self): #Esta funcion debe crearse en funciones.py e importarse 
+        tablero = np.full([self.dimensiones,self.dimensiones]," ")
+        print(tablero)
+        return tablero
+        
+    def asignar_id (self):
+        id_jugador = str(uuid.uuid1())
+        id_jugador = str(np.random.randint(0,9,6))
+
+    def colocar_barco_random(self):
+# Durante el desarrollo queremos poder verlo para comprobar que se está ejecutando ok, cuando iniciemos el juego incluiermos un argumento 
+# como mostrar_contrincante = False para que no se vea
+        print(tablero_actual) 
+        return tablero_actual
+        
+
+    def colocar_barco_jugador(self, barcos, colocar_barco):
+
+        print(tablero_actual)
+        return tablero_actual
+
+    def disparar (self):
+
+
+class Barco: 
+    def __init__ (self, eslora, n_barcos, n_impactos, posicion, orientacion):
+        self.eslora = eslora
+        self.n_barcos = n_barcos
+        self.posicion = posicion
+        self.n_impactos = n_impactos
+        self.orientacion = orientacion
+
+    
+
+
 id_jugadores =
 tablero = np.full((lado, lado), ' ')
 barcos = 
--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
-id_jugador
+asignar_id
 crear_tablero
-colocar_barco
+colocar_barco_jugador
 colocar_barco_random
 disparar(marcar si hay impacto o agua)
 
