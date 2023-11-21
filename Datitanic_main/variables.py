b90c8e285584855860417e50d7c8358c2a0f97cc
SamuelSeuru <148683583+SamuelSeuru@users.noreply.github.com>
2023-11-21 14:32:08 -0500

    Update variables.py

--- Datitanic_main/variables.py
+++ Datitanic_main/variables.py
@@ -1,1 +1,11 @@
+#Variables constantes del juego que serán utilizadas para el diseño de programación del juego
+agua = "~"
+barco = 'B'
+barco_tocado = "X"
+disparo_al_mar = '^'
+medida_tablero= 10
+
+##Diccionario para crear los barcos 
+## (x,y) -> x por numero de barcos de este tipo, y por eslora
+barcos = {"Barco de 1 eslora": (4, 1), "Barco de 2 esloras" : (3, 2), "Barco de 3 esloras": (2, 3), "Barco de 4 esloras" : (1, 4)}
 
